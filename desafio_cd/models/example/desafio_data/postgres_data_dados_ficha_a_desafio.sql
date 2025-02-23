7daecbc707805dd9eeef4fdc3f5d37dc35104686
Nayara Dornelas <nayaradornelas@id.uff.br>
2025-02-23 17:40:30 -0300

    bug fixes

--- desafio_cd/models/example/desafio_data/postgres_data_dados_ficha_a_desafio.sql
+++ desafio_cd/models/example/desafio_data/postgres_data_dados_ficha_a_desafio.sql
@@ -2,8 +2,4 @@ with source_desafio_data as (
     select * from {{ source("desafio_cd", "dados_ficha_a_desafio") }}
 )
 
-final as(
-    select * from source_desafio_data
-)
-
 select * from source_desafio_data
