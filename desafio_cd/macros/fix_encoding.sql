63ae27a8aace2da4d4e4194dd8d44d9f774cf7e7
Nayara Dornelas <nayaradornelas@id.uff.br>
2025-02-23 22:42:59 -0300

    refactor macros fix_encoding para lidar com json array vazio

--- desafio_cd/macros/fix_encoding.sql
+++ desafio_cd/macros/fix_encoding.sql
@@ -1,12 +1,16 @@
 {% macro fix_encoding(columns) %}
     {% for column in columns %}
         CASE
-          -- caso a coluna estea no formato json, converte para uma string sql text  
-            WHEN jsonb_typeof({{ column }}::jsonb) = 'array' THEN 
+            -- caso sea um JSON converte para uma string formatada
+            WHEN jsonb_typeof({{ column }}::jsonb) = 'array' AND jsonb_array_length({{ column }}::jsonb) > 0 THEN
                 array_to_string(ARRAY(
                     SELECT jsonb_array_elements_text({{ column }}::jsonb)
                 ), ', ')
-             -- caos seja texto corrompido, conrrige o encoding 
+
+            -- caso seja um JSON array vazio converte para null
+            WHEN jsonb_typeof({{ column }}::jsonb) = 'array' THEN NULL 
+
+            -- caso seja texto corrompido, corrige o encoding
             ELSE convert_from({{ column }}::bytea, 'UTF8')
         END AS {{ column }}
         {% if not loop.last %}, {% endif %}
