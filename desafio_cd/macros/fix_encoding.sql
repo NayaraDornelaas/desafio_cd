de2ca7b7f1157e6dd74780fe0d3639550f17b8a1
Nayara Dornelas <nayaradornelas@id.uff.br>
2025-02-23 22:33:04 -0300

    refactor: atualização da  macro fix_encoding para corrigir o encoding de dados tanto no formato JSON como texto

--- desafio_cd/macros/fix_encoding.sql
+++ desafio_cd/macros/fix_encoding.sql
@@ -1,6 +1,14 @@
 {% macro fix_encoding(columns) %}
     {% for column in columns %}
-        convert_from({{ column }}::bytea, 'UTF8') AS {{ column }}
+        CASE
+          -- caso a coluna estea no formato json, converte para uma string sql text  
+            WHEN jsonb_typeof({{ column }}::jsonb) = 'array' THEN 
+                array_to_string(ARRAY(
+                    SELECT jsonb_array_elements_text({{ column }}::jsonb)
+                ), ', ')
+             -- caos seja texto corrompido, conrrige o encoding 
+            ELSE convert_from({{ column }}::bytea, 'UTF8')
+        END AS {{ column }}
         {% if not loop.last %}, {% endif %}
     {% endfor %}
 {% endmacro %}
